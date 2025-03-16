fa5a2e7c065959e1d5523dc1b8c5d009539d579d
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-03-16 18:11:29 +0900

    feat: Order 엔티티추가

--- src/main/java/practice/architecture/hexagonal/domain/Order.java
+++ src/main/java/practice/architecture/hexagonal/domain/Order.java
@@ -1,4 +1,20 @@
 package practice.architecture.hexagonal.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    private String productName;
+
+    private String customerName;
+
+    private boolean orderStatus;
 }
